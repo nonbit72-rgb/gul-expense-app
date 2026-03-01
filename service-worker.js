b2676e4f9ed63ee0e00151b2075967b1fca00612
nonbit72-rgb <nonbit72@gmail.com>
2026-03-01 09:32:04 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = "et-cache-v1";
+
 const urlsToCache = [
   "./",
   "./index.html",
@@ -6,6 +7,7 @@ const urlsToCache = [
   "https://cdn.jsdelivr.net/npm/chart.js"
 ];
 
+// Install event
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -13,6 +15,22 @@ self.addEventListener("install", event => {
   );
 });
 
+// Activate event (DELETE OLD CACHES HERE 🔥)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);  // delete old
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
